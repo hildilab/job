5d548b7737c9483af0618db28ee7dab71e063bb7
Jokaso <devnull@localhost>
2014-10-07 15:13:42 +0200

    STATUS: running system - pre-extra-deletion

--- app/job_flask.py
+++ app/job_flask.py
@@ -11,6 +11,9 @@ import signal
 import logging
 import multiprocessing
 from cStringIO import StringIO
+import uuid
+import collections
+import zipfile
 
 try:
     import json
@@ -23,11 +26,14 @@ from flask import send_file
 from flask import request
 from flask import make_response, Response
 from flask import url_for, redirect
+from flask import jsonify
+from werkzeug import secure_filename
 
-from job import (
-    Job_Status, Job_Params, Job_Download, Job_Tools, Job_Submit
-)
 
+RUNNING_JOBS = {}
+logging.basicConfig( level=logging.DEBUG )
+LOG = logging.getLogger( 'job' )
+LOG.setLevel( logging.DEBUG )
 
 cfg_file = 'app.cfg'
 if len( sys.argv ) > 1:
@@ -314,6 +320,104 @@ def save_download():
 # job handling
 ############################
 
+def job_done( jobname, tool_list ):
+    LOG.info( "JOB DONE: %s - %s" % (jobname, tool_list[0].output_dir) )
+    RUNNING_JOBS[ jobname ] = False
+
+def call( tool ):
+    try:
+        tool()
+    except Exception as e:
+        print e
+    return tool
+
+
+def job_start( jobname, tool, JOB_POOL ):
+    LOG.info( "JOB STARTED: %s - %s" % (jobname, tool.output_dir) )
+    RUNNING_JOBS[ jobname ] = True
+    JOB_POOL.map_async(
+        call, [ tool ], callback=functools.partial( job_done, jobname )
+    )
+
+def input_path( name, params, output_dir ):
+    ext = params.get("ext", "dat")
+    return os.path.join( output_dir, "input_%s.%s" % ( name, ext ) )
+
+def job_dir( jobname, app, create=False ):
+    output_dir = os.path.join( app.config['JOB_DIR'], jobname )
+    output_dir = os.path.abspath( output_dir )
+    if not os.path.exists( output_dir ):
+        os.makedirs( output_dir )
+    return output_dir
+
+def _job_submit( is_form, app, JOB_POOL ):
+    # raise Exception("foo")
+
+    def get( name, params ):
+        default = params.get( "default", "" )
+        attr = "form" if is_form else "args"
+        if params.get( "nargs" ) or params.get( "action" ) == "append":
+            d = getattr( request, attr ).getlist( name + "[]" )
+            if params.get( "nargs" ) and \
+                    params.get( "action" ) == "append":
+                d = [ x.split() for x in d ]
+            if not d:
+                d = default
+        else:
+            d = getattr( request, attr ).get( name, default )
+        return d
+    jobtype = get( '__type__', {} )
+    Tool = app.config['TOOLS'].get( jobtype )
+    if Tool:
+        jobname = jobtype + "_" + str( uuid.uuid4() )
+        output_dir = job_dir( jobname, app, create=True )
+        args = []
+        kwargs = {}
+        for name, params in Tool.args.iteritems():
+            if params.get("group"):
+                continue
+            if params["type"] == "file":
+                fpath = input_path( name, params, output_dir )
+
+                for file_storage in request.files.getlist( name ):
+                    if file_storage:
+                        file_storage.save( fpath )
+                        break   # only save the first file
+                else:
+                    print "file '%s' not found, trying url" % name
+                    url = get( name, params )
+                    d = retrieve_url( url )
+                    with open( fpath, "w" ) as fp:
+                        fp.write( d )
+
+                d = str( fpath )
+            elif params["type"] == "float":
+                d = float( get( name, params ) )
+            elif params["type"] == "int":
+                d = int( get( name, params ) )
+            elif params["type"] == "bool":
+                d = boolean( get( name, { "default": False } ) )
+            elif params["type"] in [ "str", "sele" ]:
+                d = str( get( name, params ) )
+            elif params["type"] == "list":
+                d = get( name, params )
+            else:
+                # unknown type, raise exception?
+                d = get( name, params )
+                print "unknown type", d
+            if "default" in params:
+                kwargs[ name ] = d
+            else:
+                args.append( d )
+        args = tuple(args)
+        kwargs.update({
+            "output_dir": output_dir, "run": False,
+            #"verbose": True, "debug": True
+        })
+        job_start( jobname, Tool( *args, **kwargs ), JOB_POOL  )
+        return jsonify({ "jobname": jobname })
+    return "ERROR"
+
 
 # !important - allows one to abort via CTRL-C
 signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -323,23 +427,94 @@ JOB_POOL = multiprocessing.Pool( nworkers, maxtasksperchild=nworkers )
 
 @app.route('/job/status/<string:jobname>')
 def job_status( jobname ):
-    return Job_Status( jobname, app ) 
+    jobname = secure_filename( jobname )
+    jobtype, jobid = jobname.split("_")
+    Tool = app.config['TOOLS'].get( jobtype, None )
+    if Tool:
+        output_dir = job_dir( jobname, app )
+        tool = Tool( output_dir=output_dir, fileargs=True, run=False )
+        return jsonify({
+            "running": RUNNING_JOBS.get( jobname, False ),
+            "check": tool.check( full=False ),
+            "log": tool.get_full_log()
+        })
+    return ""
+
+@app.route('/job/getfile/<string:jobname>/<string:filename>')
+def job_get_file( jobname, filename ):
+    jobname = secure_filename( jobname )
+    filename = secure_filename( filename )
+    jobtype, jobid = jobname.split("_")
+    Tool = app.config['TOOLS'].get( jobtype, None )
+    if Tool:
+        output_dir = job_dir( jobname, app )
+        tool = Tool( output_dir=output_dir, fileargs=True, run=False )
+        for f in tool.output_files:
+            if filename == os.path.basename(f):
+                return send_file(
+                    f,
+                    attachment_filename="%s" % os.path.basename(f),
+                    as_attachment=True
+                )
+    return ""
 
 @app.route('/job/params/<string:jobname>')
 def job_params( jobname ):
-    return Job_Params(jobname, app)
+    jobname = secure_filename( jobname )
+    jobtype, jobid = jobname.split("_")
+    Tool = app.config['TOOLS'].get( jobtype, None )
+    if Tool:
+        output_dir = job_dir( jobname, app )
+        tool = Tool( output_dir=output_dir, fileargs=True, run=False )
+        return jsonify( tool.params )
+    return ""
 
 @app.route('/job/download/<string:jobname>')
 def job_download( jobname ):
-    return Job_Download( jobname, app )
+    jobname = secure_filename( jobname )
+    jobtype, jobid = jobname.split("_")
+    Tool = app.config['TOOLS'].get( jobtype, None )
+    if Tool:
+        output_dir = job_dir( jobname, app )
+        tool = Tool( output_dir=output_dir, fileargs=True, run=False )
+        fp = tempfile.NamedTemporaryFile( "w+b" )
+
+        with zipfile.ZipFile(fp, 'w', zipfile.ZIP_DEFLATED) as fzip:
+            for f in tool.output_files:
+                fzip.write( f, os.path.relpath( f, output_dir ) )
+        return send_file(
+            fp.name,
+            attachment_filename="%s.zip" % jobname,
+            as_attachment=True
+        )
+    return ""
+
 
 @app.route('/job/tools')
 def job_tools():
-    return Job_Tools(app)
+    tools = collections.defaultdict( dict )
+    for name, Tool in app.config['TOOLS'].iteritems():
+        tools[ name ][ 'args' ] = Tool.args
+        attr = {}
+        if hasattr( Tool, "provi_tmpl" ):
+            attr[ 'provi_file' ] = Tool.provi_tmpl
+        tools[ name ][ 'attr' ] = attr
+        tools[ name ][ 'docu' ] = Tool.__doc__
+    return jsonify( tools )
 
 @app.route('/job/submit/', methods=['POST', 'GET'])
 def job_submit():
-    return Job_Submit(app, JOB_POOL)
+    is_form = request.args.get("POST") != "_PNGJBIN_"
+    print "is_form: " + str(is_form)
+    print request.args
+    print request.form
+    print request.json
+
+    try:
+        return _job_submit( is_form, app, JOB_POOL )
+    except Exception as e:
+        print e
+        return "ERROR"
 
 ############################
 # CORS handling (by http://coalkids.github.io/author/christophe-serafin.html)
